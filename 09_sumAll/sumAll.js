8e423219f67f3387d68901a8a19fbe6683b50277
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-11-13 19:40:38 -0300

    Completed 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let numArr = [];
 
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            numArr.push(i);
+        }
+        return numArr.reduce((total, num) => total + num);
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            numArr.push(i);
+        }
+        return numArr.reduce((total, num) => total + num);  
+    }
 };
 
 // Do not edit below this line
