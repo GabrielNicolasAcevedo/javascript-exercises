708d0b9a94f05770af5377788117ba05348b4810
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-11-13 19:00:16 -0300

    Completed 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...number) {
+    
+    return array.filter(
+        item => !number.includes(item)
+    )
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
