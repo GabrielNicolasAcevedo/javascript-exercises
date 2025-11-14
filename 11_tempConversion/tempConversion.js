fd459a2ba2c6b1738f4bc0f45ebe87478388cbc7
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-11-13 21:02:57 -0300

    Completed 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahren) {
+  let temp = ((tempFahren - 32 ) * (5/9));
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  let temp = ((tempCel * (9/5)) + 32);
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
