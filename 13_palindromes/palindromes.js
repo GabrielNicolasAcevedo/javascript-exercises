8952cb3c0b0003b56a5218d4316fb3988fcc56e1
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-12-05 16:35:08 -0300

    Added comments to palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string
+        //Making it insensitive
+        .toLowerCase()
+        // Match all that are not a lowercase letter or digit and replace with ''
+        // g flag to make it global, otherwise it will take the first match
+        .replace(/[^a-z0-9]/g, '')
+    let reversedString = cleanString
+        .split("")
+        .reverse()
+        //join methods transforms an array to string
+        .join("");
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
